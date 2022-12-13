f5be52474426d626cceae181dc430572ddcfd842
William Gehrke <williamegehrke@gmail.com>
2022-12-13 15:21:36 -0500

    mobile styling

--- src/Components/Chapstick/index.js
+++ src/Components/Chapstick/index.js
@@ -23,7 +23,7 @@ function Chapstick() {
           <Typography gutterBottom  component="div" color="#e2ad22">
             <h2 className="chapstickH2">Chapstick</h2>
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography className="chapstickText" variant="body2" color="text.secondary">
           When people find out that we are honey bee farming, they always ask about the honey. But bees also produce beeswax and something will need to be done with the beeswax when you harvest honey. We take this beeswax and filter out the impurities using a simple method of boiling in water and filtering. From there we just mix the beeswax with 1 parts beeswax and 2 parts coconut oil and a few drops of vitamin e. All the ingredients are organic. </Typography>
         </CardContent>
      
